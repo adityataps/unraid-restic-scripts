e5cd8d597a9d95414cd95ed6241454f7b8126555
Aditya Tapshalkar <aditya.taps@gmail.com>
2025-01-16 23:05:47 -0500

    fix: reuse tee wrap

--- backup.sh
+++ backup.sh
@@ -6,17 +6,17 @@ ENV_FILE_PATH="$SCRIPT_DIR/.env"
 # shellcheck source=.env
 touch "$ENV_FILE_PATH" && source "$ENV_FILE_PATH"
 
-exec > >(tee -a "$SCRIPT_DIR/logs/cron.log") 2>&1
-printf '\n\n\n------------------------------------------------------------------------------------------\n'
-date
-printf '\n=====\n'
+{
+  printf '\n\n\n------------------------------------------------------------------------------------------\n'
+  date
+  printf '\n=====\n'
 
-### 1. Setup logrotate
-mkdir -p "$SCRIPT_DIR/logs"
-if [[ -f /etc/logrotate.d/restic ]]; then
-  echo "Logrotate configuration for Restic already exists."
-else
-  cat >/etc/logrotate.d/restic <<EOF
+  ### 1. Setup logrotate
+  mkdir -p "$SCRIPT_DIR/logs"
+  if [[ -f /etc/logrotate.d/restic ]]; then
+    echo "Logrotate configuration for Restic already exists."
+  else
+    cat >/etc/logrotate.d/restic <<EOF
 $SCRIPT_DIR/logs/cron.log {
 	weekly
 	rotate 4
@@ -27,59 +27,60 @@ $SCRIPT_DIR/logs/cron.log {
 	create 644 root root
 }
 EOF
-  echo "Logrotate configured for Restic."
-fi
+    echo "Logrotate configured for Restic."
+  fi
 
-### 2. Backup
-echo 'Backing up shares to Restic repository...'
-docker run \
-  --rm \
-  --name='restic-backup' \
-  --net='homelab' \
-  --pids-limit 2048 \
-  -e TZ="America/New_York" \
-  -e HOST_OS="Unraid" \
-  -e HOST_HOSTNAME="HomeLab" \
-  -e HOST_CONTAINERNAME="restic" \
-  -e RESTIC_REPOSITORY="$RESTIC_REPOSITORY" \
-  -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
-  -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
-  -l net.unraid.docker.managed=dockerman \
-  -l net.unraid.docker.icon='https://raw.githubusercontent.com/nwithan8/unraid_templates/master/images/restic-icon.png' \
-  -v '/mnt/user/appdata/restic/password':'/pass':'rw' \
-  -v '/mnt/user/aditya/':'/data/aditya/':'rw' \
-  -v '/mnt/user/lydia/':'/data/lydia/':'rw' \
-  -v '/mnt/user/shared/':'/data/shared/':'rw' \
-  --hostname unraid 'restic/restic:latest' \
-  --password-file /pass backup /data || {
-  echo 'Could not back up to Restic.'
-  exit 1
-}
-echo 'Finished backing up shares to Restic repository.'
+  ### 2. Backup
+  echo 'Backing up shares to Restic repository...'
+  docker run \
+    --rm \
+    --name='restic-backup' \
+    --net='homelab' \
+    --pids-limit 2048 \
+    -e TZ="America/New_York" \
+    -e HOST_OS="Unraid" \
+    -e HOST_HOSTNAME="HomeLab" \
+    -e HOST_CONTAINERNAME="restic" \
+    -e RESTIC_REPOSITORY="$RESTIC_REPOSITORY" \
+    -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+    -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+    -l net.unraid.docker.managed=dockerman \
+    -l net.unraid.docker.icon='https://raw.githubusercontent.com/nwithan8/unraid_templates/master/images/restic-icon.png' \
+    -v '/mnt/user/appdata/restic/password':'/pass':'rw' \
+    -v '/mnt/user/aditya/':'/data/aditya/':'rw' \
+    -v '/mnt/user/lydia/':'/data/lydia/':'rw' \
+    -v '/mnt/user/shared/':'/data/shared/':'rw' \
+    --hostname unraid 'restic/restic:latest' \
+    --password-file /pass backup /data || {
+    echo 'Could not back up to Restic.'
+    exit 1
+  }
+  echo 'Finished backing up shares to Restic repository.'
 
-### 3. Prune backups
-echo 'Pruning backups...'
-docker run \
-  --rm \
-  --name='restic-prune' \
-  --net='homelab' \
-  --pids-limit 2048 \
-  -e TZ="America/New_York" \
-  -e HOST_OS="Unraid" \
-  -e HOST_HOSTNAME="HomeLab" \
-  -e HOST_CONTAINERNAME="restic" \
-  -e RESTIC_REPOSITORY="$RESTIC_REPOSITORY" \
-  -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
-  -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
-  -l net.unraid.docker.managed=dockerman \
-  -l net.unraid.docker.icon='https://raw.githubusercontent.com/nwithan8/unraid_templates/master/images/restic-icon.png' \
-  -v '/mnt/user/appdata/restic/password':'/pass':'rw' \
-  -v '/mnt/user/aditya/':'/data/aditya/':'rw' \
-  -v '/mnt/user/lydia/':'/data/lydia/':'rw' \
-  -v '/mnt/user/shared/':'/data/shared/':'rw' \
-  --hostname unraid 'restic/restic:latest' \
-  --password-file /pass forget --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --prune || {
-  echo 'Could not prune Restic backups.'
-  exit 1
-}
-echo 'Pruned backups.'
+  ### 3. Prune backups
+  echo 'Pruning backups...'
+  docker run \
+    --rm \
+    --name='restic-prune' \
+    --net='homelab' \
+    --pids-limit 2048 \
+    -e TZ="America/New_York" \
+    -e HOST_OS="Unraid" \
+    -e HOST_HOSTNAME="HomeLab" \
+    -e HOST_CONTAINERNAME="restic" \
+    -e RESTIC_REPOSITORY="$RESTIC_REPOSITORY" \
+    -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+    -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+    -l net.unraid.docker.managed=dockerman \
+    -l net.unraid.docker.icon='https://raw.githubusercontent.com/nwithan8/unraid_templates/master/images/restic-icon.png' \
+    -v '/mnt/user/appdata/restic/password':'/pass':'rw' \
+    -v '/mnt/user/aditya/':'/data/aditya/':'rw' \
+    -v '/mnt/user/lydia/':'/data/lydia/':'rw' \
+    -v '/mnt/user/shared/':'/data/shared/':'rw' \
+    --hostname unraid 'restic/restic:latest' \
+    --password-file /pass forget --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --prune || {
+    echo 'Could not prune Restic backups.'
+    exit 1
+  }
+  echo 'Pruned backups.'
+} | tee -a /boot/config/scripts/restic/logs/cron.log 2>&1
